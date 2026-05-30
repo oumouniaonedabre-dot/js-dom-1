b230d61a3a9f90891faf5056d30e59cb96c882e0
oumouniaonedabre-dot <oumouniaonedabre@gmail.com>
2026-05-30 22:19:10 +0200

    css completato

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 let immagine = document.querySelector('#imgAttiva');
 let bottone = document.querySelector('#btnAccendi');
 let accesa = false;
+
+bottone.addEventListener('click', () => {
+    if (!accesa) {
+         immagine.src = 'img/yellow_lamp.png';
+        bottone.textContent = 'Spegni';
+        accesa = true;
+         } 
+         
+    else {
+        immagine.src = 'img/white_lamp.png';
+        bottone.textContent = 'Accendi';
+        accesa = false;
+    }
+});
+
